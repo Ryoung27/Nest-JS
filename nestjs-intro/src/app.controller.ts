f62642757baa5e3fa2012838239c7f9e32800869
Ronnie Young <ronnie.young@plainode.io>
2019-08-10 08:16:12 -0500

    post product set up

--- nestjs-intro/src/app.controller.ts
+++ nestjs-intro/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // This is what you send back to the host
+  // It auto generates a header
+  // You can create your own header like below.
+  //WOW it auto generated the import of a Header above.
+  @Header('Content-Type', 'text/html')
+  getHello(): {name: string} {
+    return {name: 'Richie'};
   }
 }
