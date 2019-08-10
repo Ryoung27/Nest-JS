3334c9bbe1c55be234dd7dc822bf9aefb01c81c6
Ronnie Young <ronnie.young@plainode.io>
2019-08-10 09:54:51 -0500

    returning a single product

--- nestjs-intro/src/products/products.controller.ts
+++ nestjs-intro/src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param } from "@nestjs/common";
 
 import { ProductsService } from './products.service';
 
@@ -14,5 +14,14 @@ export class ProductsController{
         ): any {
         const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
         return {id: generatedId}
+    }
+    @Get()
+    getAllProducts(){
+        return this.productsService.getProducts();
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') prodId: string,){
+        return this.productsService.getSingleProducts(prodId);
     }
 }
