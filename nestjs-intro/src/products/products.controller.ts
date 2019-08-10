0fbf8ef18a7004f24acec22d1a115c3559fd8754
Ronnie Young <ronnie.young@plainode.io>
2019-08-10 12:28:05 -0500

    complete nest js api

--- nestjs-intro/src/products/products.controller.ts
+++ nestjs-intro/src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param, Patch, Delete } from "@nestjs/common";
 
 import { ProductsService } from './products.service';
 
@@ -23,5 +23,22 @@ export class ProductsController{
     @Get(':id')
     getProduct(@Param('id') prodId: string,){
         return this.productsService.getSingleProducts(prodId);
+    }
+    //I should use Put instead of Patch
+    @Patch(':id')
+    updateProduct(
+      @Param('id') prodId: string,
+      @Body('title') prodTitle: string,
+      @Body('description') prodDesc: string,
+      @Body('price') prodPrice: number,
+    ) {
+      this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+      return null;
+    }
+  
+    @Delete(':id')
+    removeProduct(@Param('id') prodId: string) {
+        this.productsService.deleteProduct(prodId);
+        return null;
     }
 }
