91fb047ec9b565f928eb7573b1c31a953515a751
Ronnie Young <ronnie.young@plainode.io>
2019-08-11 08:22:34 -0500

    finished NestJS API

--- nestjs-intro/src/products/products.controller.ts
+++ nestjs-intro/src/products/products.controller.ts
@@ -1,47 +1,58 @@
-import { Controller, Post, Body, Get, Param, Patch, Delete } from "@nestjs/common";
-
-import { ProductsService } from './products.service';
-
-@Controller('products')
-export class ProductsController{
-    constructor(private readonly productsService: ProductsService){}
-
+import {
+    Controller,
+    Post,
+    Body,
+    Get,
+    Param,
+    Patch,
+    Delete,
+  } from '@nestjs/common';
+  
+  import { ProductsService } from './products.service';
+  
+  @Controller('products')
+  export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {}
+  
     @Post()
-    addProduct(
-        @Body('title') prodTitle: string, 
-        @Body('description') prodDesc: string, 
-        @Body('price') prodPrice: number,
-        ): any {
-        const generatedId = this.productsService.insertProduct(
-            prodTitle, 
-            prodDesc, 
-            prodPrice);
-        return {id: generatedId}
+    async addProduct(
+      @Body('title') prodTitle: string,
+      @Body('description') prodDesc: string,
+      @Body('price') prodPrice: number,
+    ) {
+      const generatedId = await this.productsService.insertProduct(
+        prodTitle,
+        prodDesc,
+        prodPrice,
+      );
+      return { id: generatedId };
     }
+  
     @Get()
-    getAllProducts(){
-        return this.productsService.getProducts();
+    async getAllProducts() {
+      const products = await this.productsService.getProducts();
+      return products;
     }
-
+  
     @Get(':id')
-    getProduct(@Param('id') prodId: string,){
-        return this.productsService.getSingleProducts(prodId);
+    getProduct(@Param('id') prodId: string) {
+      return this.productsService.getSingleProduct(prodId);
     }
-    //I should use Put instead of Patch
+  
     @Patch(':id')
-    updateProduct(
+    async updateProduct(
       @Param('id') prodId: string,
       @Body('title') prodTitle: string,
       @Body('description') prodDesc: string,
       @Body('price') prodPrice: number,
     ) {
-      this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+      await this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
       return null;
     }
   
     @Delete(':id')
-    removeProduct(@Param('id') prodId: string) {
-        this.productsService.deleteProduct(prodId);
+    async removeProduct(@Param('id') prodId: string) {
+        await this.productsService.deleteProduct(prodId);
         return null;
     }
-}
+  }
